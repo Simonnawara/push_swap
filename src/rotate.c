6f7dd5981e800d0c50b83618bd307286b1b44770
Simon <sim.nawara@gmail.com>
2024-11-04 11:29:33 +0100

    added rotate

--- src/rotate.c
+++ src/rotate.c
@@ -10,4 +10,41 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "push_swap.h"
+#include "../includes/push_swap.h"
+
+static void	rotate(t_stack **stack)
+{
+	t_stack	*first;
+	t_stack	*temp;
+
+	if (!stack || !*stack || !(*stack)->next)
+		return ;
+	first = *stack;
+	*stack = (*stack)->next;
+	first->next = NULL;
+
+	// Traverse to the end and append `first` there
+	temp = *stack;
+	while (temp->next)
+		temp = temp->next;
+	temp->next = first;
+}
+
+void	ra(t_stack **stack_a)
+{
+	rotate(*stack_a);
+	ft_putendl_fd("ra", 1);
+}
+
+void	rb(t_stack **stack_b)
+{
+	rotate(*stack_b);
+	ft_putendl_fd("rb", 1);
+}
+
+void	ss(t_stack **stack_a, t_stack **stack_b)
+{
+	rotate(*stack_a);
+	rotate(*stack_b);
+	ft_putendl_fd("rr", 1);
+}
